9dce98b5d7581202f29ef48b61f9941db18fdaae
Yuzori <161332657+Yuzori@users.noreply.github.com>
2024-07-21 21:18:47 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const socket = io('https://sarlextracoiffure.onrender.com');
+const path = require('path');
+const schedule = require('node-schedule');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+});
+
+const reservations = new Map(); // Stockage des réservations en mémoire
+
+io.on('connection', (socket) => {
+    console.log('Un client s\'est connecté');
+
+    // Envoyer les réservations existantes au client lorsqu'il se connecte
+    socket.emit('initialReservations', Array.from(reservations.values()));
+
+    socket.on('newReservation', (reservation) => {
+        console.log('Nouvelle réservation reçue:', reservation);
+
+        const reservationTime = new Date(`2000-01-01T${reservation.time}`);
+        const endTime = new Date(reservationTime.getTime() + 10 * 60000);
+
+        let isTimeSlotAvailable = true;
+
+        for (let [time, existingReservation] of reservations) {
+            const existingEndTime = new Date(time.getTime() + 10 * 60000);
+            if (
+                (reservationTime >= time && reservationTime < existingEndTime) ||
+                (endTime > time && endTime <= existingEndTime) ||
+                (reservationTime <= time && endTime >= existingEndTime)
+            ) {
+                isTimeSlotAvailable = false;
+                break;
+            }
+        }
+
+        if (isTimeSlotAvailable) {
+            reservation.id = Date.now().toString();
+            reservations.set(reservationTime, reservation);
+            io.emit('newReservation', reservation);
+            console.log('Réservation acceptée, envoi de la réponse');
+            socket.emit('reservationResponse', {
+                success: true
+            });
+        } else {
+            console.log('Réservation refusée, envoi de la réponse');
+            socket.emit('reservationResponse', {
+                success: false,
+                message: "Une réservation pour cette heure est déjà en cours. Veuillez choisir un autre horaire."
+            });
+        }
+    });
+
+    socket.on('deleteReservation', (id) => {
+        for (let [time, reservation] of reservations) {
+            if (reservation.id === id) {
+                reservations.delete(time);
+                io.emit('reservationDeleted', id);
+                break;
+            }
+        }
+    });
+
+    socket.on('deleteAllReservations', () => {
+        reservations.clear();
+        io.emit('allReservationsDeleted');
+    });
+});
+
+// Planifier la suppression des réservations tous les jours à 17h
+schedule.scheduleJob('0 17 * * *', () => {
+    reservations.clear();
+    io.emit('allReservationsDeleted');
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+    console.log(`Serveur en écoute sur le port ${PORT}`);
+});
